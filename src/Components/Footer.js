f9edcd2045b1d53ba4579a803d8cf7b2d316ad45
Scott Pribe <Scottpribe@gmail.com>
2021-01-30 15:27:10 -0500

    footer fully responsive

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,12 +6,22 @@ import Twitter from "../ImagesAndIcons/icons8-twitter-24.png";
 const Footer = () => {
 	return (
 		<div id="footer">
-			<div id="addressContainer">
-				{" "}
-				<h3>5147 S Main St. Sylvania, OH 43560 -- 419-517-0233</h3>
-				<h3>25481 N Dixie Hwy, Perrysburg, OH 43551 -- 419-874-8800</h3>
+			<div className="footerDivs" id="sylvaniaLocationContainer">
+				<h3>Sylvania Location</h3>
+				<p>5147 S Main St. Sylvania, OH 43560</p>
+				<p>419-517-0233</p>
 			</div>
-			<div id="socialsContainer">
+			<div className="footerDivs" id="perrysburgLocationContainer">
+				<h3>Perrysburg Location</h3>
+				<p>25481 N Dixie Hwy, Perrysburg, OH 43551</p>
+				<p>419-874-8800</p>
+			</div>
+			<div className="footerDivs" id="hoursOfOperation">
+				<h3>Hours</h3>
+				<p>Sunday - Thursday: 11:00AM - 9:30PM</p>
+				<p>Friday - Saturday: 11:00AM - 10:00PM</p>
+			</div>
+			<div className="footerDivs" id="socialsContainer">
 				<h3>Follow Us</h3>
 				<ul id="socialsList">
 					<li>
