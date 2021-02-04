93f2f1677d8064ef2908017e468ab4d8e4a50f5b
Scott Pribe <Scottpribe@gmail.com>
2021-02-04 15:20:48 -0500

    need to add final touches. make navbar float at the top, cleanup SCSS file, load PDF faster, fixed the webworker error, fix the route 404 error whenever it's in build.

--- src/Components/Menu/Drink.js
+++ src/Components/Menu/Drink.js
@@ -1,126 +1,27 @@
-import React from "react";
-import SectionNavbar from "./SectionNavbar";
-// import tequila from "../../Data/tequila.js";
-// import margaritas from "../../Data/margaritas.js";
+import React, { useState, useLayoutEffect } from "react";
 import drinkMenu from "../../tekela_drink_menu.pdf";
 import { Document, Page } from "react-pdf";
 import { pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Drink = () => {
-	// const items = (subcategory) => {
-	// 	if (subcategory === "silvers")
-	// 		return tequila.silvers.map((name) => <li>{name}</li>);
-	// 	else if (subcategory === "reposados")
-	// 		return tequila.reposados.map((name) => <li>{name}</li>);
-	// 	else if (subcategory === "anejos")
-	// 		return tequila.anejos.map((name) => <li>{name}</li>);
-	// 	else if (subcategory === "reservas")
-	// 		return tequila.reservas.map((name) => <li>{name}</li>);
-	// 	else if (subcategory === "margaritas")
-	// 		return margaritas.map((object) => (
-	// 			<li>
-	// 				<h4>{object.name}</h4>
-	// 				<p>{object.description}</p>
-	// 			</li>
-	// 		));
-	// };
+	const [scale, setScale] = useState(1);
+
+	useLayoutEffect(() => {
+		if (window.innerWidth >= 955) setScale(1.5);
+		else if (window.innerWidth >= 365 && window.innerWidth < 955) setScale(1);
+		else setScale(0.8);
+	}, []);
 
 	return (
 		<div id="drinkMenuSectionDiv">
-			<div className="menu">
+			<div className="menu" id="drinkMenu">
 				<Document file={drinkMenu} loading={"loading..."}>
 					{[1, 2, 3, 4, 5, 6, 7, 8].map((page) => (
-						<Page scale={1.5} pageNumber={page} loading={""} />
+						<Page scale={scale} pageNumber={page} loading={""} />
 					))}
 				</Document>
 			</div>
-			{/* <SectionNavbar menuType="Drinks"></SectionNavbar>
-
-			<section className="drinkMenuSections" id="Tequila">
-				<h2>Tequila</h2>
-				<ul>
-					<h3>Silvers</h3>
-					{items("silvers")}
-				</ul>
-				<ul>
-					<h3>Reposados</h3>
-					{items("reposados")}
-				</ul>
-				<ul>
-					<h3>Anejos</h3>
-					{items("anejos")}
-				</ul>
-				<ul>
-					<h3>Reservas</h3>
-					{items("reservas")}
-				</ul>
-				<div>
-					<h2>Flights</h2>
-					<p>Three 1/2 OZ Glasses of Specialty Tequila for Sampling</p>
-					<p>We Recommend Trying One of Two Options:</p>
-					<p>
-						One Brand with Three Ages or One Age with Three Different Brands.
-					</p>
-				</div>
-			</section>
-			<section className="drinkMenuSections" id="Margaritas">
-				<h2>Margaritas</h2>
-				<ul> {items("margaritas")}</ul>
-			</section>
-			<section className="drinkMenuSections" id="Cocktails" value="Cocktails">
-				<div>
-					<h2>Cocktails</h2>
-					<ul>
-						<h3>Everyday Cocktails</h3>
-					</ul>
-					<ul>
-						<h3>Seasonal Cocktails</h3>
-					</ul>
-				</div>
-			</section>
-
-			<section className="drinkMenuSections" id="Beer">
-				<h2>Beer</h2>
-				<div>
-					<h3>Draft Beer</h3>
-					<ul></ul>
-				</div>
-
-				<div>
-					<h3>Bottled Beer</h3>
-					<ul>
-						<h3>Imported</h3>
-						<h3>Domestic</h3>
-					</ul>
-				</div>
-				<div>
-					<h2>Cans</h2>
-				</div>
-			</section>
-			<section className="drinkMenuSections" id="Wine">
-				<div>
-					<h2>Wine</h2>
-					<ul>
-						<h2>White Wines</h2>
-					</ul>
-					<ul>
-						<h2>Red Wines</h2>
-					</ul>
-				</div>
-			</section>
-			<section className="drinkMenuSections" id="Mojitos">
-				<div>
-					<h2>Mojitos</h2>
-					<ul></ul>
-				</div>
-			</section>
-			<section className="drinkMenuSections" id="Sangria">
-				<div>
-					<h2>Sangria</h2>
-					<ul></ul>
-				</div>
-			</section> */}
 		</div>
 	);
 };
--- src/Components/Menu/Food.js
+++ src/Components/Menu/Food.js
@@ -1,5 +1,4 @@
 import React, { useState, useLayoutEffect } from "react";
-import SectionNavbar from "./SectionNavbar";
 import foodMenu from "../../tekela_dinner_menu.pdf";
 
 import { Document, Page } from "react-pdf";
@@ -7,55 +6,25 @@ import { pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Food = () => {
-	const [scale, setScale] = useState(() => {
-		if (window.innerWidth >= 955) {
-			return 1.5;
-		} else if (window.innerWidth >= 628 && window.innerWidth < 955) {
-			return 1;
-		} else if (window.innerWidth >= 450 && window.innerWidth < 627) {
-			return 0.8;
-		} else return 0.6;
-	});
-
-	// const setPageScale = () => {
-	// 	if
-	// };
+	const [scale, setScale] = useState(1);
 
 	useLayoutEffect(() => {
-		console.log("change the scale yo!");
-	});
+		if (window.innerWidth >= 955) setScale(1.2);
+		else if (window.innerWidth >= 628 && window.innerWidth < 955) setScale(1);
+		else if (window.innerWidth >= 450 && window.innerWidth < 627) setScale(0.8);
+		else if (window.innerWidth > 340 && window.innerWidth < 450) setScale(0.6);
+		else setScale(0.5);
+	}, []);
 
 	return (
 		<div id="foodMenuSectionDiv">
-			{/* <SectionNavbar menuType="Food"></SectionNavbar> */}
 			<div className="menu" id="foodMenu">
 				<Document file={foodMenu} loading={"loading..."}>
 					{[2, 3, 4, 5, 6, 7].map((page) => (
-						<Page
-							scale={scale}
-							pageNumber={page}
-							loading={""}
-							// onLoadSuccess={setPageScale}
-						/>
+						<Page scale={scale} pageNumber={page} loading={""} />
 					))}
 				</Document>
 			</div>
-			{/* 
-			<section className="foodMenuSections" value="Appetizers">
-				<h2>Appetizers</h2>
-			</section>
-			<section className="foodMenuSections" value="Lunch">
-				<h2>Lunch</h2>
-			</section>
-			<section className="foodMenuSections" value="Dinner">
-				<h2>Dinner</h2>
-			</section>
-			<section className="foodMenuSections" value="Kids Menu">
-				<h2>Kids Menu</h2>
-			</section>
-			<section className="foodMenuSections" value="Desserts">
-				<h2>Desserts</h2>
-			</section> */}
 		</div>
 	);
 };
